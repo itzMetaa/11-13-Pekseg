6205c44541b91a00d08196644b4ecdfa3bbfebcb
Diak <Diak@10.4.117.15>
2019-11-13 15:04:17 +0100

    init

---
+++ src/peksegoop/Arlap.java
@@ -0,0 +1,9 @@
+
+package peksegoop;
+
+
+public abstract interface Arlap {
+    public static final double CSESZEKAVE=180;
+    
+    public abstract double mennyibeKerul();
+}
---
+++ src/peksegoop/PeksegOOP.java
@@ -0,0 +1,10 @@
+
+package peksegoop;
+
+public class PeksegOOP {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
